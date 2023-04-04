cfcf0cb2b3ef02f6d5d97713298f09ac15ba93ba
JulianColoma <juliancoloma751@gmail.com>
2023-04-04 16:30:22 -0300

    creacion del login y routing

--- Portfolio/front end/src/app/app.module.ts
+++ Portfolio/front end/src/app/app.module.ts
@@ -14,6 +14,9 @@ import { HardSoftComponent } from './components/hard-soft/hard-soft.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { FooterComponent } from './components/footer/footer.component' ;
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -26,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HardSoftComponent,
     ProyectsComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
