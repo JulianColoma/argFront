299f80ca1d82c16f37e4ed8a7ceaed386a480af8
JulianColoma <juliancoloma751@gmail.com>
2023-04-25 18:49:00 -0300

    implementacion en front error de compilacion

--- Portfolio/back end/C/src/main/java/com/Julian/C/Controller/PersonaController.java
+++ Portfolio/back end/C/src/main/java/com/Julian/C/Controller/PersonaController.java
@@ -6,6 +6,7 @@ import com.Julian.C.Entity.Persona;
 import com.Julian.C.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,17 +25,19 @@ public class PersonaController {
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
      }
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
        ipersonaService.savePersona(persona);
        return "la persona fue creada correctamente";
     }
-
+ @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "la persona fue eliminada correctamente";
     }
+     @PreAuthorize("hasRole('ADMIN')")
     // URL:puerto/personas/editar/id/nombre & apellido & img
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
