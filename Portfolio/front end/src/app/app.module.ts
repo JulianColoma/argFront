5c37b84e7e7d8b56c8ff2dbc6a1c2bd6877e1e53
JulianColoma <juliancoloma751@gmail.com>
2023-05-02 19:59:17 -0300

    componente experiencia terminado (editar borrar y agragar)

--- Portfolio/front end/src/app/app.module.ts
+++ Portfolio/front end/src/app/app.module.ts
@@ -18,6 +18,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { interceptorProvider } from './service/interceptor-service';
 import {FormsModule} from '@angular/forms';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,9 @@ import {FormsModule} from '@angular/forms';
     ProyectsComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
     
   ],
   imports: [
